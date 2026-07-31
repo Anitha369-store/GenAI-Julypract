6a82ae07d324fe301c910de9b5b773dba561d682
ashivama <anitha.bit@gmail.com>
2026-07-31 22:35:11 +0530

    saving

---
+++ github.md/add.py
@@ -0,0 +1,9 @@
+num1 = int(input("enter your first number:"))
+num2 = int(input("enter your second number:"))
+
+sum = num1 + num2
+print("Sum=", sum)
+
+diff = num1 - num2
+print("Diff =" , diff)
+
