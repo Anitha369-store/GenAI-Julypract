36fe0bab9a7621f87e05ec4dde0ccd8a50572fb9
ashivama <anitha.bit@gmail.com>
2026-08-06 19:44:44 +0530

    conditional statement saved

---
+++ github.md/Python/conditioning.py
@@ -0,0 +1,119 @@
+# calculator application
+
+#input("This is your mini calculator")
+
+#num1 = int(input("Enter the  1st number "))
+#num2 = int(input("Enter the 2nd number "))
+
+#sum = num1 + num2
+
+#print("Sum of 2 numbers is : ", sum)
+
+#diff = num1 - num2
+
+#print("diff os 2 numbers is :", diff)
+
+#produc = num1 * num2
+
+#print (" product of 2 numbers are " , produc)
+
+#div = num1 / num2
+
+
+
+#print("division is  ", div)
+
+
+
+
+# with condition
+
+#age = int(input("enter your age"))
+
+#if age >= 18:
+ #   print("you are eligible")
+
+#else:
+ #   print("not eligible")
+
+
+
+
+#balance = float(input("Enter your balance"))
+
+#if balance <=20000:
+ #   print("you get loan")
+
+#else:
+ #   print("you are not elegible")
+
+
+
+#number = int(input("Enter the number  "))
+#if (number % 2) == 0:
+ #   print("number is even")
+#else:
+ #   print("number is odd")
+
+
+#mark = int(input("Enter the marks "))
+#if mark >= 90:
+ #   print("Grade A")
+
+#elif mark >=80:
+ #   print("gradeB")
+
+#elif mark >=70:
+ #   print("grade C")
+
+#else:
+ #   print(" student is failed ")
+    
+
+
+
+
+#from unittest import case
+
+
+#num1 = int(input("enter 1st number. : "))
+
+#num2 = int(input("enter 2st number. : "))
+
+#operator = input("enter the operator you wantto perform(+ , - , *, / )")
+
+#match operator:
+ #   case"+":
+  #      print("Result is  ", num1 + num2 )
+
+   # case"-":
+    #    print("Result is ", num1 - num2 )
+
+    #case"*":
+     #print("Result is  ", num1 * num2)
+
+
+    #case"/" :
+     #   print("Result is ", num1/num2)
+
+
+
+
+import string
+
+
+#color = string(input("Enter the color"))
+
+
+
+operate = input("Enter the color (Red,Green,Yellow) ")
+
+match operate:
+    case"Red" :
+        print("STOP" )
+    case"Yellow":
+        print("slow")
+    case"Green":
+        print("GO" )
+
+        
